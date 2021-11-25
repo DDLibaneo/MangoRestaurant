9895ed5fe9089f7c90dcfa85744e723f2cc751e7
ddlibaneo <daniel.libaneo@gmail.com>
2021-11-25 00:13:10 -0300

    wip migration seed

---
+++ Mango.Services.ProductAPI/Migrations/20211125030248_FirstSeeds.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.ProductAPI.Migrations
+{
+    public partial class FirstSeeds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
