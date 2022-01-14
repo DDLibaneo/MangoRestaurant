a09e83d6e1e8b2f111dbc284110d3e44d784e210
ddlibaneo <daniel.libaneo@gmail.com>
2022-01-13 21:37:13 -0300

    add unit test project

---
+++ Mango.Services.ProductAPI.Tests/ProductControllerTest.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using Mango.Services.ProductAPI;
+
+namespace Mango.Services.ProductAPI.Tests
+{
+    public class ProductControllerTest
+    {
+        //resolver as dependencias do teste
+
+        [Fact]
+        public void GetAll()
+        {
+
+        }
+    }
+}
